38cf89cb50f6195e93d19f0ddc67c9d5b5206156
Philip M. Gollucci <pgollucci@p6m7g8.com>
2025-10-14 17:21:52 -0400

    chore: moves from mergify to github queue

---
+++ p6-stratosphere.zsh
@@ -0,0 +1,17 @@
+######################################################################
+#<
+#
+# Function: p6df::modules::p6cirrus::init()
+#
+#  Depends:	 p6_bootstrap
+#  Environment:	 P6_DFZ_SRC_ORIGINAL_DIR
+#>
+######################################################################
+p6df::modules::p6-stratosphere::init() {
+  local _module="$1"
+  local dir="$2"
+
+  p6_bootstrap "$dir"
+
+  p6_return_void
+}
